860d7693c19e908efa7e1c0ab8c3285b2bcd7efd
ralucagrama <raluca.tritean@quadom.com>
2024-09-21 18:40:46 +0300

    first commit

---
+++ src/components/SidebarDate.js
@@ -0,0 +1,30 @@
+import CV from '../images/cv.png';
+import React from 'react'
+
+export const SidebarData=[
+    {
+        title: "CV",
+        icon: <img className='iconImage' src={CV} alt="CV Icon" />,
+        link:"/cv"
+    },
+    {
+        title: "Shop Project",
+        icon: <img className='iconImage' src={CV} alt="CV Icon" />,
+        link:"/shop"
+    },
+    {
+        title: "x Project",
+        icon: <img className='iconImage' src={CV} alt="CV Icon" />,
+        link:"/xProj"
+    },
+    {
+        title: "Y Project",
+        icon: <img className='iconImage' src={CV} alt="CV Icon" />,
+        link:"/yProj"
+    },
+    {
+        title: "V Project",
+        icon: <img className='iconImage' src={CV} alt="CV Icon" />,
+        link:"/home"
+    }
+]
